398c00e2b0893356f9e1d0c2881eee1fe9055415
Artemmakhlay <116097849+Artemmakhlay@users.noreply.github.com>
2022-11-16 14:42:16 -0500

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,34 @@
+function Jaune () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+}
+function Rouge () {
+    pins.digitalWritePin(DigitalPin.P2, 1)
+}
+function Orange () {
+    pins.digitalWritePin(DigitalPin.P8, 1)
+}
+input.onButtonPressed(Button.A, function () {
+    Vert()
+    basic.pause(10000)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    Jaune()
+    basic.pause(5000)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    Rouge()
+    basic.pause(100)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    Blanc()
+    basic.pause(15000)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    Orange()
+    basic.pause(5000)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    Vert()
+})
+function Vert () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+}
+function Blanc () {
+    pins.digitalWritePin(DigitalPin.P16, 1)
+}
+Vert()
